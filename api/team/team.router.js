bfa9ae59b7458acf4f728d4a236bd2d0887299f6
pa526 <p7266634@gmail.com>
2025-11-29 21:12:08 +0530

    29/11/25 modified team.router.js, app.js

--- api/team/team.router.js
+++ api/team/team.router.js
@@ -2,6 +2,11 @@ const {createTeamValidation} = require("../../validation/team/team.validation");
 const {getAllTeam} = require("../team/team.controller");
 const router = require("express").Router();
 
-router.get("all/", createTeamValidation, getAllTeam);
+router.get("/all", (req, res) => {
+    res.json({
+        msg: "this is working",
+    })
+   console.log("app is working");
+});
 
 module.exports = router; 
--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const app = express();
 const teamRouter = require("./api/team/team.router");
+const app = express();
 
 app.use(express.json());
 app.use(cors({
@@ -10,3 +10,9 @@ app.use(cors({
 }));
 
 app.use("api/team", teamRouter);
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+    console.log(`app is listening on port ${port}`);
+});
